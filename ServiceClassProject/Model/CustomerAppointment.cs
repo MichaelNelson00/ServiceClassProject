05ae8656ee848c8e32ccace7df61097ebceaa860
MichaelNelson00 <michael.p.nelson@marquette.edu>
2023-11-30 13:24:58 -0600

    Create Appointment and CustomerAppointment classes

---
+++ ServiceClassProject/Model/CustomerAppointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceClassProject.Model;
+
+public class CustomerAppointment
+{
+    public Customer customer { get; set; } //must have a customer and appointment
+    public Appointment appointment { get; set; }
+
+    public CustomerAppointment(Customer c, Appointment a)
+    {
+        customer = c;
+        appointment = a;
+    }
+}
